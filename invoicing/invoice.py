975c6c48a036ff3e6425c7af18d28ae7ae6a2843
richardadu-poku <richardjadupoku@gmail.com>
2024-01-30 14:54:27 -0500

    Testing the Package

--- invoicing/invoice.py
+++ invoicing/invoice.py
@@ -8,6 +8,18 @@ from pathlib import Path
 
 def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amount_purchased,
              price_per_unit, total_price):
+    """
+    This function converts invoice Excel files into PDF invoices.
+    :param invoices_path:
+    :param pdfs_path:
+    :param image_path:
+    :param product_id:
+    :param product_name:
+    :param amount_purchased:
+    :param price_per_unit:
+    :param total_price:
+    :return:
+    """
     filepaths = glob.glob(f"{invoices_path}/*.xlsx")
 
     for filepath in filepaths:
@@ -63,5 +75,6 @@ def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amo
         pdf.cell(w=25, h=8, txt="PythonHow")
         pdf.image(image_path, w=10)
 
-        os.makedirs(pdfs_path)
+        if not os.path.exists(pdfs_path):
+            os.makedirs(pdfs_path)
         pdf.output(f"{pdfs_path}/{filename}.pdf")
